91ea9efe7360e17680a0605c6d9ee8d33a3110a4
Slaven Tepsic <slaven92@gmail.com>
2020-04-08 02:38:59 +0200

    fixed redirects

--- kviz/urls.py
+++ kviz/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('kviz/', views.kviz, name='kviz'),
     path('result/', views.result, name='result'),
+    path('process/', views.process, name='process'),
 ]
--- kviz/views.py
+++ kviz/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_list_or_404, get_object_or_404
+from django.shortcuts import render, get_list_or_404, get_object_or_404, redirect
 from django.http import HttpResponse
 import glob
 import os
@@ -12,38 +12,21 @@ from .models import Question, Choice
 
 
 def index(request):
+    request.session['question_number'] = 1
+    request.session['result'] = []
     return render(request, 'kviz/index.html', {})
 
 def kviz(request):
-    try:
-        if(request.method == 'GET'):
-            request.session['question_number'] = 1
-            request.session['result'] = []
-        elif(request.method == 'POST'):
-            ch = get_object_or_404(Choice, pk=request.POST['choice'])
-            request.session['result'].append(ch.choice_text)
-            request.session['question_number'] += 1
-    except (KeyError):
-        context = create_contex_for_kviz(request, 'Nema preskakanja!')
-        return render(request, 'kviz/kviz.html', context)
-    else:
+    if(request.method == 'GET'):
         context = create_contex_for_kviz(request,"")
         return render(request, 'kviz/kviz.html', context)
+    else:
+        return redirect('kviz:kviz')
 
 def result(request):
     if(request.method == 'GET'):
-        error = 'odradi kviz prvo'
-        result = ''
-    elif(request.method == 'POST'):
-        try:
-            request.session['result'].append(request.POST['choice'])
-        except (KeyError):
-            context = create_contex_for_kviz(request, 'Nema preskakanja!')
-            return render(request, 'kviz/kviz.html', context)
-        else:
-            error = ''
-
-            
+        num_of_questions = len(get_list_or_404(Question))
+        if request.session['question_number'] == num_of_questions:
             # logic here
             images = IM_DIR+'*.jpeg'
             image_list = glob.glob(images)
@@ -51,13 +34,42 @@ def result(request):
             image_list_names = [ os.path.splitext(p)[0] for p in image_list_base ]
             #======================
             result = random.choice(image_list_names)
-
             #===========
-    context = { 'result': result,
-                'error' : error,
-                'user_results': request.session['result']}
-    return render(request, 'kviz/result.html', context)
+            context = { 'result': result,
+                        'error' : '',
+                        'user_results': request.session['result']}
+            request.session['result'] = []
+            request.session['question_number'] = 1
+            return render(request, 'kviz/result.html', context)
+        else:
+            context = { 'result': [],
+                        'error' : 'Prvo odradi kviz',
+                        'user_results': []}
+            return render(request, 'kviz/result.html', context)
+    else:
+        return redirect('kviz:kviz')
 
+
+def process(request):
+    if(request.method == 'GET'):
+        return redirect('kviz:kviz')
+    elif(request.method == 'POST'):
+        try:
+            ch = get_object_or_404(Choice, pk=request.POST['choice'])
+        except (KeyError):
+            context = create_contex_for_kviz(request, 'Nema preskakanja!')
+            return render(request, 'kviz/kviz.html', context)
+        else:
+            num_of_questions = len(get_list_or_404(Question))
+            request.session['result'].append(ch.choice_text)
+            if request.session['question_number'] == num_of_questions:
+                request.session.modified = True
+                return redirect('kviz:result')
+            request.session['question_number'] += 1
+    return redirect('kviz:kviz')
+
+
+## helper functions
 def create_contex_for_kviz(request, error):
     question_number = request.session.get('question_number', 1)
     question_list = get_list_or_404(Question)
@@ -69,3 +81,4 @@ def create_contex_for_kviz(request, error):
                 'error': error,
                 'percent':str(percent),}
     return context
+
