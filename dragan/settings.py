6563d15bb3f1fd955ca28e2192840f92a6ae25e0
Slaven Tepsic <slaven92@gmail.com>
2020-07-22 17:56:10 +0200

    h787

--- dragan/settings.py
+++ dragan/settings.py
@@ -165,4 +165,4 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
