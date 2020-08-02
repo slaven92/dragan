0df40f76d236aeb89d90f38ae732ba3b707686b0
Slaven Tepsic <slaven92@gmail.com>
2020-08-02 17:51:41 +0200

    added support for promise

--- kviz/schema.py
+++ kviz/schema.py
@@ -2,6 +2,7 @@ from graphene import relay, ObjectType, Mutation, Field, ID, List
 from graphene import Int, String, Boolean, InputObjectType
 from graphene_django.types import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
+from graphene_django import DjangoConnectionField
 from graphql_relay import from_global_id
 
 from django.contrib.auth.models import User
@@ -11,61 +12,52 @@ import os, glob, random
 from graphene_django.debug import DjangoDebug
 
 
-# from promise import Promise
-# from promise.dataloader import DataLoader
-# from collections import defaultdict
+from promise import Promise
+from promise.dataloader import DataLoader
+from collections import defaultdict
+from django.core.cache import cache
 
-# class ChoiceLoader(DataLoader):
-#     def batch_load_fn(self, question_keys):
-#         # Here we return a promise that will result on the
-#         # corresponding user for each key in keys
+class ChoiceLoader(DataLoader):
+    def batch_load_fn(self, question_keys):
+        # Here we return a promise that will result on the
+        # corresponding user for each key in keys
 
-#         choices = defaultdict(list)
+        choices = defaultdict(list)
 
 
-#         for choice in Choice.objects.filter(question_id__in=question_keys).iterator():
-#             choices[choice.question_id].append(choice)
+        for choice in Choice.objects.filter(question_id__in=question_keys).iterator():
+            choices[choice.question_id].append(choice)
         
-#         print(choices)
+        print(choices)
 
 
-#         return Promise.resolve([choices.get(question_id, []) for question_id in question_keys])
+        return Promise.resolve([choices.get(question_id, []) for question_id in question_keys])
 
-# choice_loader = ChoiceLoader()
+choice_loader = ChoiceLoader()
+
+class VotesLoader(DataLoader):
+    def batch_load_fn(self, question_keys):
+        # Here we return a promise that will result on the
+        # corresponding user for each key in keys
+
+        votes = defaultdict(list)
+
+        
+        for key in question_keys:
+            for user in Question.objects.get(pk=key).votes.all():
+                votes[key].append(user)
+        
+        print(votes)
+
+
+        return Promise.resolve([votes.get(question_id, []) for question_id in question_keys])
+
+vote_loader = VotesLoader()
+
+
 
 DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 IM_DIR = os.path.join(DIR, 'kviz/static/kviz/')
-
-class QuestionNode(DjangoObjectType):
-    """
-    Question Node gives information about questions
-    """
-
-    vote_count = Int()
-    user_did_vote = Boolean()
-
-    class Meta:
-        model = Question
-        filter_fields = ['question_text']
-        interfaces = (relay.Node, )
-
-    def resolve_vote_count(self, info):
-        return self.votes.count()
-
-    def resolve_votes(self, info):
-        if not info.context.user.is_authenticated:
-            return User.objects.none()
-        
-        return self.votes.all()
-
-    def resolve_user_did_vote(self, info):
-        if not info.context.user.is_authenticated:
-            return False
-
-        return self.votes.filter(pk=info.context.user.id).exists()
-
-    # def resolve_choice_set(self, info):
-    #     return choice_loader.load(self.id)
 
 
 class ChoiceNode(DjangoObjectType):
@@ -90,9 +82,63 @@ class UserNode(DjangoObjectType):
         fields = ('username',)
         interfaces = (relay.Node, )
 
+
+class QuestionNode(ObjectType):
+    """
+    Question Node gives information about questions
+    """
+
+    question_text = String()
+    votes = DjangoConnectionField(UserNode)
+    creator = Field(UserNode)
+    choice_set = DjangoConnectionField(ChoiceNode)
+
+    vote_count = Int()
+    user_did_vote = Boolean()
+
+    class Meta:
+        interfaces = (relay.Node, )
+
+    def resolve_vote_count(self, info):
+        return vote_loader.load(self.id).then(lambda votes: len(votes))
+
+    def resolve_votes(self, info):
+        if not info.context.user.is_authenticated:
+            return User.objects.none()
+
+        return  vote_loader.load(self.id)
+
+    def resolve_user_did_vote(self, info):
+        if not info.context.user.is_authenticated:
+            return False
+
+        return vote_loader.load(self.id).then(lambda votes: info.context.user in votes )
+
+    def resolve_choice_set(self, info):
+        return choice_loader.load(self.id)
+
+    def resolve_question_text(self, info):
+        return self.question_text
+
+    def resolve_creator(self, info):
+        return self.creator
+
+    @classmethod
+    def get_node(cls, info, id):
+        return Question.objects.get(pk=id)
+
+
+class QuestionConnection(relay.Connection):
+    class Meta:
+        node = QuestionNode
+
+
+
+
+
 class Query(ObjectType):
     question = relay.Node.Field(QuestionNode)
-    all_questions = DjangoFilterConnectionField(QuestionNode)
+    all_questions = relay.ConnectionField(QuestionConnection)
 
     choice = relay.Node.Field(ChoiceNode)
     # all_choices = DjangoFilterConnectionField(ChoiceNode)
@@ -102,8 +148,16 @@ class Query(ObjectType):
 
     is_logged_in = Boolean()
 
-    def resolve_is_logged_in(parent, info):
+    def resolve_is_logged_in(self, info):
         return info.context.user.is_authenticated
+
+    def resolve_all_questions(self, info):
+        questions = cache.get("questions")
+        if not questions:
+            questions = Question.objects.all()
+            cache.set("questions", questions)
+        
+        return questions
 
 
 class VoteMutation(relay.ClientIDMutation):
