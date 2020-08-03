7a0f3fdb5f3d315f7ffbab401c9c9c7e419af2cb
Slaven Tepsic <slaven92@gmail.com>
2020-08-03 10:43:25 +0200

    forgot to change the app.sh to use with openshift

--- app.sh
+++ app.sh
@@ -2,4 +2,6 @@
 
 # python manage.py migrate
 
-daphne -b 0.0.0.0 -p 8000 --access-log - --proxy-headers dragan.asgi:application
+gunicorn -b 0.0.0.0:8000 -w 4 -k uvicorn.workers.UvicornWorker dragan.asgi:application
+
+# daphne -b 0.0.0.0 -p 8000 --access-log - --proxy-headers dragan.asgi:application
